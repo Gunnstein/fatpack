10ecc2a9661c6dcfd4eedd708501d24ba304567b
Gunnstein Thomas Froeseth <gunnsteinthomas@gmail.com>
2018-06-29 16:13:30 +0200

    Bumped version number for the documentation

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name='fatpack',
-      version='0.5',
+      version='0.5.1',
       url='https://github.com/Gunnstein/FatPACK',
       license='MIT License',
       description='Package for fatigue analysis, FatPACK',
